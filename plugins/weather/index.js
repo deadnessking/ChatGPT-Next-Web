e62127c0a51928d006fd64d52e870fc3a728e036
deadnessking <40329411+deadnessking@users.noreply.github.com>
2023-07-07 16:34:56 +0800

    Create index.js

---
+++ plugins/weather/index.js
@@ -0,0 +1,7 @@
+// index.js
+module.exports = {
+  name: "Weather",
+  description: "A ChatGPT plugin to get the weather of any given location",
+  icon: "https://weather.vercel.app/icon.png",
+  version: "1.0.0",
+};
